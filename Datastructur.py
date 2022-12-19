b889ccfc703e12a977126cda33befc9f4f27c636
yasirmoo <120780160+yasirmoo@users.noreply.github.com>
2022-12-19 16:40:02 +0700

    Add files via upload

---
+++ Datastructur.py
@@ -0,0 +1,9 @@
+from array import *
+bilangan1 = array('i',[10,20,30,40,50]) #Data utama yang akan olah
+print (bilangan1[0])
+print (bilangan1[1])
+formula1 = bilangan1[0] + bilangan1[1] * bilangan1[0] #Rumus hitungan, akan di tarik untuk hasil perhitungan
+formula2 = bilangan1[1] + bilangan1[1] * bilangan1[0] 
+
+print (bilangan1[0],"+",bilangan1[1],"*",bilangan1[0],"=",formula1) #tugas menampilkan string dan hasil penjumlahan
+print (bilangan1[1],"+",bilangan1[1],"*",bilangan1[0],"=",formula1)
